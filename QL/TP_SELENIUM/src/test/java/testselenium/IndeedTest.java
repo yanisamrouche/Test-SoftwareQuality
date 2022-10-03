dc7f94524ba1c176167c6493d89cc3e578b076ea
Yanis Amrouche <54064644+yanisamrouche@users.noreply.github.com>
2022-10-03 22:59:45 +0200

    selenium -> indeed

--- QL/TP_SELENIUM/src/test/java/testselenium/IndeedTest.java
+++ QL/TP_SELENIUM/src/test/java/testselenium/IndeedTest.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class IndeedTest
 {
     public static void main(String[] args) {
@@ -30,17 +32,55 @@ public class IndeedTest
 
 // Vérifier que le titre de la page est celui obtenu
         if (titreObtenu.contentEquals(titreAttendu)){
-            logger.info("Succès");
+            logger.info("Succès => le titre de la page est celui obtenu");
         } else {
             logger.info("Echec");
         }
-// Récupérer l'id = jobsearch
+// vérifie que la page web contient un formulaire dont l’id est jobsearch.
         WebElement jobsearch = driver.findElement(By.id("jobsearch"));
         if(jobsearch.getTagName().equals("form")){
-            logger.info("Succès");
+            logger.info("Succès => la page web contient un formulaire dont l’id est jobsearch");
         }else {
             logger.info("Echec");
         }
+// Vérifier que l'élément h2 contient la chaine "recherches populaires"
+        String chaineAttendu = "Recherches populaires";
+        List<WebElement> listH = driver.findElements(By.tagName("h2"));
+        for (WebElement webElement : listH){
+            if (webElement.getText().equals(chaineAttendu)){
+                logger.info("Succès => l'élément h2 contient la chaine 'recherches populaires'");
+                break;
+            }
+        }
+// Vérifier que la page contient un lien hypertexte dont le texte est « À propos »
+        String apropos = "À propos";
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        for(WebElement link : links){
+            if (link.getText().contentEquals(apropos)){
+                logger.info("Succès => la page contient un lien hypertexte dont le texte est « À propos »");
+                break;
+            }
+        }
+//Vérifier que la page contient un lien hypertexte dont une partie du texte est « Guide »
+        String guide = "Guide";
+        List<WebElement> links_ = driver.findElements(By.tagName("a"));
+        for(WebElement link : links_){
+            if (link.getText().contains(guide)){
+                logger.info("Succès => la page contient un lien hypertexte dont une partie du texte est « Guide »");
+                break;
+            }
+        }
+//Vérifier que la page contient un champ de formulaire dont le nom est q.
+        String q = "q";
+        List<WebElement> forms = driver.findElements(By.tagName("form"));
+        for (WebElement form : forms){
+            if (form.getText().toLowerCase().contains("q")){
+                logger.info("Succès => la page contient un champ de formulaire dont le nom est q.");
+                break;
+            }
+        }
+
+
 // Quitter le navigateur
         driver.close();
     }
